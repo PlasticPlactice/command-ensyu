07e8420ec36a80f31ae3ff9e166ff16668b4e96e
Hatano Yuto <y.hatano.sys23@morijyobi.ac.jp>
2023-11-09 10:48:25 +0900

    Main.javaの名前をBmi.javaに変更しました。

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("BMI計算ツールです。(成人版)");
